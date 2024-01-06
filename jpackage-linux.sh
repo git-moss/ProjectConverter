39f5cb2f572e10b4259a20ac4edb2b843df8e8a1
mos <mos@mac-mini-von-jurgen.fritz.box>
2024-01-06 13:35:18 +0100

    Added Macos + Linux build files.

---
+++ jpackage-linux.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/home/mos/java/jdk-17.0.9+9
+mvn clean install package jpackage::jpackage@linux
---
+++ jpackage-mac-arm.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home
+mvn clean install package jpackage::jpackage@mac
